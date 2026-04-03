fcf97df1d40895d130f8dbd7127659745771e8a8
ckdash-git <developer@optionallabs.com>
2026-04-03 18:58:18 +0530

    Initial Commit

---
+++ MindSync/MindSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindSyncApp.swift
+//  MindSync
+//
+//  Created by CHANDAN on 03/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MindSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
